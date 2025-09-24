bcc4bfd474269a36cf87380c371d76c354655ec3
Zahidan Ardhiansyah <zahidan23@gmail.com>
2025-09-24 17:31:41 +0700

    Update : styling header top container

--- app.config.ts
+++ app.config.ts
@@ -1,6 +1,6 @@
 export default defineAppConfig({
   ui: {
-    primary: 'orange',
+    primary: 'orange-500',
     button: {
       rounded: 'rounded-sm',
       padding: {
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,11 +1,43 @@
 <template>
-  <div>
-    <header>Header</header>
-    <main><slot /></main>
+  <div class="default-layout">
+    <header class="header-orange">
+      <div class="header-top">
+        <UContainer class="header-top-container">
+          <span>Login</span>
+        </UContainer>
+      </div>
+      <div class="header-bottom"></div>
+    </header>
+
+    <main class="main-layout">
+      <slot />
+    </main>
+
     <footer>Footer</footer>
   </div>
 </template>
 
-<script setup></script>
+<style>
+@reference "tailwindcss";
 
-<style lang="scss" scoped></style>
+.default-layout {
+  @apply flex flex-col min-h-screen bg-red-200;
+}
+
+.main-layout {
+  @apply flex-1 bg-slate-300;
+}
+
+.header-orange {
+  @apply text-white flex flex-col;
+  height: 120px;
+}
+
+.header-orange .header-top {
+  @apply bg-orange-700 py-2;
+}
+
+.header-top-container {
+  @apply flex justify-end;
+}
+</style>
