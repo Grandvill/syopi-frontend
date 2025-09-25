2c3fd5ee5e64e0e5076f75fa67f9f452623e5c68
Zahidan Ardhiansyah <zahidan23@gmail.com>
2025-09-25 13:19:17 +0700

    Update : gradient color header top and bottom container

--- layouts/default.vue
+++ layouts/default.vue
@@ -3,11 +3,17 @@
     <header class="header-orange">
       <div class="header-top">
         <UContainer class="header-top-container">
-          <UButton color="gray" variant="ghost" label="Daftar" />
-          <UButton color="gray" variant="ghost" label="Login" />
+          <UButton color="white" variant="link" :padded="false" label="Daftar" />
+          <UButton color="white" variant="link" :padded="false" label="Login" />
         </UContainer>
       </div>
-      <div class="header-bottom"></div>
+      <div class="header-bottom">
+        <UContainer class="header-bottom-container">
+          <div>Logo</div>
+          <div>Search Bar</div>
+          <div>Cart Button</div>
+        </UContainer>
+      </div>
     </header>
 
     <main class="main-layout">
@@ -35,10 +41,21 @@
 }
 
 .header-orange .header-top {
-  @apply bg-orange-700 py-2;
+  @apply bg-orange-600 py-2;
 }
 
 .header-top-container {
   @apply flex justify-end;
+  @apply divide-x divide-gray-50/50;
+}
+
+.header-orange .header-bottom {
+  @apply bg-gradient-to-b from-orange-600 to-[#FF6633];
+  @apply flex-1;
+}
+
+.header-bottom-container {
+  @apply flex justify-between items-center;
+  @apply h-full;
 }
 </style>
