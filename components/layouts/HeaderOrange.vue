2e4fa5bccb6a1028e9528becad51b41977f97b55
Zahidan Ardhiansyah <zahidan23@gmail.com>
2025-09-29 14:57:20 +0700

    Add : dynamic layout header

--- components/layouts/HeaderOrange.vue
+++ components/layouts/HeaderOrange.vue
@@ -1,24 +1,33 @@
 <template>
   <header class="header-orange">
-    <LayoutsHeaderProfile />
+    <LayoutsHeaderProfile v-if="defaultMeta.showProfile" />
     <div class="header-bottom">
       <UContainer class="header-bottom-container">
         <BaseLogo />
-        <LayoutsSearchBar class="flex-1" />
-        <UButton>
-          <IconCart variant="link" />
+        <LayoutsSearchBar v-if="defaultMeta.showSearch" class="flex-1" />
+        <UButton v-if="defaultMeta.showCart" variant="link">
+          <IconCart />
         </UButton>
       </UContainer>
     </div>
   </header>
 </template>
 
-<script setup></script>
+<script setup>
+const route = useRoute();
+
+const defaultMeta = computed(() => {
+  return {
+    showProfile: route.meta?.header && 'showProfile' in route.meta.header ? route.meta.header.showProfile : true,
+    showSearch: route.meta?.header && 'showSearch' in route.meta.header ? route.meta.header.showSearch : true,
+    showCart: route.meta?.header && 'showCart' in route.meta.header ? route.meta.header.showCart : true,
+  };
+});
+</script>
 
 <style scoped>
 .header-orange {
   @apply text-white flex flex-col;
-  height: 120px;
 }
 
 .header-orange .header-top {
@@ -33,6 +42,7 @@
 .header-orange .header-bottom {
   @apply bg-gradient-to-b from-primary to-[#FF6633];
   @apply flex-1;
+  @apply py-6;
 }
 
 .header-bottom-container {
--- components/layouts/HeaderWhite.vue
+++ components/layouts/HeaderWhite.vue
@@ -1,20 +1,24 @@
 <template>
   <header class="header-white">
-    <LayoutsHeaderProfile />
+    <LayoutsHeaderProfile v-if="route.meta.header?.showProfile" />
     <div class="header-bottom">
       <UContainer class="header-bottom-container">
         <div class="flex gap-4 items-center">
           <BaseLogo color="orange" />
-          <hr class="w-[1px] h-5 bg-primary" />
-          <p class="text-xl text-black">Tittle</p>
+          <template v-if="route.meta.header?.title">
+            <hr class="w-[1px] h-5 bg-primary" />
+            <p class="text-xl text-black">{{ route.meta.header?.title }}</p>
+          </template>
         </div>
-        <LayoutsSearchBar class="w-1/2" :padded="false" />
+        <LayoutsSearchBar v-if="route.meta.header?.showSearch" class="w-1/2" :padded="false" />
       </UContainer>
     </div>
   </header>
 </template>
 
-<script setup></script>
+<script setup>
+const route = useRoute();
+</script>
 
 <style scoped>
 @reference "tailwindcss";
