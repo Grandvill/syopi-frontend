0ee5eb3797499046e6c9972d2476860f854375a9
Zahidan Ardhiansyah <zahidan23@gmail.com>
2025-10-01 13:36:56 +0700

    Add : dropdown profile in HeaderSeller component

--- components/layouts/HeaderSeller.vue
+++ components/layouts/HeaderSeller.vue
@@ -1,6 +1,14 @@
 <template>
   <header class="header-seller">
     <UBreadcrumb :links="breadcrumb" :ui="ui" />
+    <UDropdown :items="profileItems">
+      <template #default="{ open }">
+        <div class="flex gap-2 items-center">
+          <UAvatar icon="i-heroicons:building-storefront" img-class="object-cover" />
+          <UIcon name="i-heroicons:chevron-down-20-solid" class="w-4 h-4 transition-all" :class="[open && 'rotate-180']" />
+        </div>
+      </template>
+    </UDropdown>
   </header>
 </template>
 
@@ -32,6 +40,19 @@ const ui = {
   base: 'font-normal text-lg',
   li: 'text-black/50',
 };
+
+const profileItems = [
+  [
+    {
+      label: 'Switch to Customer',
+      icon: 'i-heroicons:arrow-path',
+    },
+    {
+      label: 'Logout',
+      icon: 'i-heroicons:arrow-left-start-on-rectangle',
+    },
+  ],
+];
 </script>
 
 <style scoped>
@@ -39,5 +60,6 @@ const ui = {
   @apply bg-white px-4 py-3;
   @apply shadow;
   @apply fixed top-0 left-0 right-0 z-50;
+  @apply flex justify-between gap-2;
 }
 </style>
