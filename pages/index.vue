648c239a550c47afd094be50d3d067ef70258a32
Zahidan Ardhiansyah <zahidan23@gmail.com>
2025-10-02 12:40:52 +0700

    Add : banner section in homepage

--- pages/index.vue
+++ pages/index.vue
@@ -1,13 +1,28 @@
 <template>
-  <div>Homepage</div>
+  <div>
+    <section class="banner-section">
+      <UContainer>
+        <featureHomepageCarousel :items="items" />
+      </UContainer>
+    </section>
+    <section class="category-section"></section>
+    <section class="product-section"></section>
+  </div>
 </template>
 
 <script setup>
-definePageMeta({
-  header: {
-    showProfile: false,
-  },
-});
+const items = [
+  'https://picsum.photos/1920/1080?random=1',
+  'https://picsum.photos/1920/1080?random=2',
+  'https://picsum.photos/1920/1080?random=3',
+  'https://picsum.photos/1920/1080?random=4',
+  'https://picsum.photos/1920/1080?random=5',
+  'https://picsum.photos/1920/1080?random=6',
+];
 </script>
 
-<style lang="scss" scoped></style>
+<style scoped>
+.banner-section {
+  @apply bg-white py-7;
+}
+</style>
