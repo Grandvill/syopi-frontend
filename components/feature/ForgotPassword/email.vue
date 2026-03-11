03f9389a1687693765d05ef861f606da45e2f76f
Zahidan_A <zahidan23@gmail.com>
2026-03-11 10:30:40 +0700

    update to component

---
+++ components/feature/ForgotPassword/email.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="flex gap-7 items-center">
+        <UButton icon="i-heroicons:arrow-left" variant="link" />
+        <h3 class="text-xl font-normal">Reset Password</h3>
+    </div>
+    <div class="px-16 pb-16 pt-9">
+        <form class="space-y-6">
+            <UFormGroup>
+                <UInput placeholder="Email" size="lg" />
+            </UFormGroup>
+            <UButton block class="uppercase">Berikutnya</UButton>
+        </form>
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
--- pages/forgot-password.vue
+++ pages/forgot-password.vue
@@ -2,18 +2,7 @@
     <section class="bg-white">
         <UContainer class="py-14">
             <UCard class="max-w-[500px] mx-auto auth-shadow">
-                <div class="flex gap-7 items-center">
-                    <UButton icon="i-heroicons:arrow-left" variant="link" />
-                    <h3 class="text-xl font-normal">Reset Password</h3>
-                </div>
-                <div class="px-16 pb-16 pt-9">
-                    <form class="space-y-6">
-                        <UFormGroup>
-                            <UInput placeholder="Email" size="lg" />
-                        </UFormGroup>
-                        <UButton block class="uppercase">Berikutnya</UButton>
-                    </form>
-                </div>
+               <FeatureForgotPasswordEmail />
             </UCard>
         </UContainer>
     </section>
