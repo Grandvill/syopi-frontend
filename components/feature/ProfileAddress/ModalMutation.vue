efbe34c07dd4a64052235cd0fdc0904255a3980b
Zahidan_A <zahidan23@gmail.com>
2026-04-07 11:19:09 +0700

    Add : slicing Address - modal add address

---
+++ components/feature/ProfileAddress/ModalMutation.vue
@@ -0,0 +1,82 @@
+<template>
+    <UModal v-model="open">
+        <UCard 
+        :ui="{
+            rounded: 'rounded-sm',
+        }"
+        >
+            <h3 class="text-xl font-normal">Alamat Baru</h3>
+            <form class="grid grid-cols-2 gap-5 mt-5">
+                <UFormGroup>
+                    <UInput placeholder="Nama Lengkap" size="lg" />
+                </UFormGroup>
+                <UFormGroup>
+                    <UInput placeholder="Nomor Telepon" size="lg" />
+                </UFormGroup>
+                <UFormGroup class="col-span-2"> <!-- cols-2 jadi lebih panjang fieldnya -->
+                    <UInput placeholder="Provinsi, Kota" size="lg" />
+                </UFormGroup>
+                <UFormGroup>
+                    <UInput placeholder="Kecamatan" size="lg" />
+                </UFormGroup>
+                <UFormGroup>
+                    <UInput placeholder="Kode Pos" size="lg" />
+                </UFormGroup>
+                 <UFormGroup class="col-span-2">
+                    <UTextarea placeholder="Nama Jalan, Gedung, No. Rumah" size="lg" />
+                </UFormGroup>
+                <UFormGroup class="col-span-2">
+                    <UInput placeholder="Detail Lainnya (Cth: Blok / Unit No., Patokan)" size="lg" />
+                </UFormGroup>
+                <div class="col-span-2">
+                    <label class="text-sm font-normal text-black/80">Tandai Sebagai : </label>
+                    <div class="flex gap-2 justify-start my-2">
+                        <UButton color="white" 
+                        :ui="{
+                            padding: {
+                                sm: 'px-3 py-3',
+                            },
+                        }"
+                        >Rumah</UButton>
+
+                        <UButton color="white" 
+                        :ui="{
+                            padding: {
+                                sm: 'px-3 py-3',
+                            },
+                        }"
+                        >Kantor</UButton>
+                    </div>
+                    <UCheckbox label="Atur sebagai Alamat Pribadi" :ui="{
+                        label: 'text-black/55'
+                    }" />
+                </div>
+                <div class="flex justify-end mt-6 gap-2 col-span-2">
+                    <UButton label="Nanti Saja" color="gray" class="max-w-36" variant="link" block @click="handleCancel" />
+                    <UButton label="OK" class="max-w-36" block @click="handleSave" />
+                </div>
+            </form>
+        </UCard>
+    </UModal>
+</template>
+
+<script setup>
+const open = defineModel("open", {
+  type: Boolean,
+  default: false,
+});
+
+function handleCancel() {
+    // reset state
+    open.value = false;
+}
+
+function handleSave() {
+    //HIT API
+    open.value = false;
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
