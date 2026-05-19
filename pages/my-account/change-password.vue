495a0325f5e8c645fae34ddfb8b216685c79ae4f
Zahidan_A <zahidan23@gmail.com>
2026-05-19 14:27:55 +0700

    Add : Slicing change password

--- pages/my-account/change-password.vue
+++ pages/my-account/change-password.vue
@@ -1,10 +1,38 @@
 <template>
     <div>
-        <h1>Change Password</h1>
+        <h3 class="font-medium text-lg">Atur Password</h3>
+        <p class="text-sm text-black/65">
+            Untuk keamanan akun Anda, mohon untuk tidak menyebarkan password Anda ke orang lain.
+        </p>
+        <hr class="mt-5 mb-11 border-gray-200/50">
+        <form class="space-y-3">
+            <UFormGroup label="Password Baru" :ui="{
+                wrapper: 'flex gap-5',
+                container: 'w-96',
+                label: {
+                    wrapper: 'pt-2',
+                    base: 'text-normal text-black/55 text-right w-40'
+                }
+            }">
+                <BaseInputPassword size="lg"/>
+            </UFormGroup>
+            <UFormGroup label="Konfirmasi Password" :ui="{
+                wrapper: 'flex gap-5',
+                container: 'w-96',
+                label: {
+                    wrapper: 'pt-2',
+                    base: 'text-normal text-black/55 text-right w-40'
+                }
+            }">
+                <BaseInputPassword size="lg"/>
+                <UButton label="Konfirmasi" class="mt-8" />
+            </UFormGroup>
+        </form>
     </div>
 </template>
 
-<script>
+<script setup>
+
 </script>
 
 <style lang="scss" scoped>
