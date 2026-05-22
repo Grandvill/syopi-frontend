bc1b8d9c81a68778f8e393f1941129fe2f5bb906
Zahidan_A <zahidan23@gmail.com>
2026-05-22 15:13:13 +0700

    Add : Slicing Order List Page

---
+++ components/feature/ProfileOrder/CardProduct.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="flex gap-3">
+        <img src="https://picsum.photos/200/300" class="aspecr-[1/1] w-20 h-20 object-cover" />
+        <div class="flex-1">
+            <p class="line-clamp-2 text-base">Product Name</p>
+            <p class="text-sm text-black/55">Variasi: white, 39</p>
+            <p class="text-sm">x1</p>
+        </div>
+        <div class="flex items-center">
+            Rp{{ formatNumber(1000) }}
+        </div>
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
