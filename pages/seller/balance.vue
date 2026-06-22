6ec52b89cf9026044c686e3fc1f2b0485b48fdb5
Zahidan_A <zahidan23@gmail.com>
2026-06-22 16:07:27 +0700

    Add : Slicing Seller - Withdraw Wallet

--- pages/seller/balance.vue
+++ pages/seller/balance.vue
@@ -5,7 +5,7 @@
         <p class="text-black/65">Saldo</p>
         <div class="flex items-center gap-4">
           <span class="text-3xl font-medium">Rp{{ formatNumber(1000) }}</span>
-          <UButton>Tarik Dana</UButton>
+          <UButton @click="openWithDraw = true">Tarik Dana</UButton>
         </div>
       </div>
       <p class="text-lg font-medium">0 Transaksi</p>
@@ -13,6 +13,36 @@
         <template #amount-data="{ row }">Rp{{ formatNumber(row.amount) }}</template>
       </BaseDataTable>
     </div>
+
+    <UModal v-model="openWithDraw">
+      <UCard>
+        <div class="flex justify-between items-center">
+          <span>Tarik Dana</span>
+          <UButton icon="i-heroicons:x-mark" variant="ghost" color="grey" @click="openWithDraw = false"/>
+        </div>
+        <form class="space-y-4 mt-4">
+          <UFormGroup label="Bank">
+          <USelectMenu :options="[]" size="lg" />
+          </UFormGroup>
+          <UFormGroup label="No. Rekening">
+            <UInput size="lg" />
+          </UFormGroup>
+          <UFormGroup label="Nama Pemilik Rekening">
+            <UInput size="lg" />
+          </UFormGroup>
+          <UFormGroup label="Jumlah Penarikan">
+            <UInput type="number" size="lg" />
+          </UFormGroup>
+          <UFormGroup label="Deskripsi">
+            <UTextarea size="lg" />
+          </UFormGroup>
+          <div class="flex gap-2 items-center justify-end pt-4">
+            <UButton color="white">Tutup</UButton>
+            <UButton>Konfirmasi</UButton>
+          </div>
+        </form>
+      </UCard>
+    </UModal>
   </SellerCard>
 </template>
 
@@ -20,6 +50,8 @@
 definePageMeta({
   breadcrumb: [{ label: 'Saldo Penjual' }],
 });
+
+const openWithDraw = ref(false)
 
 const columns = [
   {
