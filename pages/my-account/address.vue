57d126b30cd026397611b55c1e936ece37ccef70
Zahidan_A <zahidan23@gmail.com>
2026-07-28 16:12:17 +0700

    feat : integrasi my account - Address - List

--- pages/my-account/address.vue
+++ pages/my-account/address.vue
@@ -9,12 +9,18 @@
         <hr class="mt-5 mb-3 border-gray-200/60" />
         <h4 class="text-lg font-normal">Alamat</h4>
         <div class="divide-y">
-            <FeatureProfileAddressCardItem
-                v-for="address in addressList"
+          <div v-if="status === 'pending'" class="space-y-2 mt-4">
+            <USkeleton class="h-4 w-[250px]" />
+            <USkeleton class="h-4 w-[200px]" />
+          </div>
+            <template v-else>
+              <FeatureProfileAddressCardItem
+                v-for="address in addressList?.data"
                 :key="address.uuid"
                 :item="address"
                 @change="handleChange"
             />
+            </template>
         </div>
 
         <FeatureProfileAddressModalMutation v-model:open="openEditor" />
@@ -23,48 +29,50 @@
 
 <script setup>
 const openEditor = ref(false);
-const addressList = [
-  {
-    uuid: "8c438746-79c5-11ef-b707-97a05ceb87e2",
-    is_default: true,
-    receiver_name: "Azis Hapidin",
-    receiver_phone: "08888",
-    city: {
-      uuid: "ee8eb26c-78fe-11ef-bd77-9e4478916c69",
-      province: {
-        uuid: "ee8d857c-78fe-11ef-bd77-9e4478916c69",
-        name: "Bali",
-      },
-      external_id: 128,
-      name: "Kabupaten Gianyar",
-    },
-    district: "Bojong",
-    postal_code: "43222",
-    detail_address: "Jl. ABC No. 123",
-    address_note: "Dekat tugu pahlawan",
-    type: "home",
-  },
-  {
-    uuid: "953bc2be-79c5-11ef-b707-97a05ceb87e2",
-    is_default: false,
-    receiver_name: "Azis Hapidin",
-    receiver_phone: "08888",
-    city: {
-      uuid: "ee8eb26c-78fe-11ef-bd77-9e4478916c69",
-      province: {
-        uuid: "ee8d857c-78fe-11ef-bd77-9e4478916c69",
-        name: "Bali",
-      },
-      external_id: 128,
-      name: "Kabupaten Gianyar",
-    },
-    district: "Bojong",
-    postal_code: "43222",
-    detail_address: "Jl. ABC No. 123",
-    address_note: "Dekat tugu pahlawan",
-    type: "home",
-  },
-];
+
+const {data: addressList, status} = useApi('/server/api/address')
+// const addressList = [
+//   {
+//     uuid: "8c438746-79c5-11ef-b707-97a05ceb87e2",
+//     is_default: true,
+//     receiver_name: "Azis Hapidin",
+//     receiver_phone: "08888",
+//     city: {
+//       uuid: "ee8eb26c-78fe-11ef-bd77-9e4478916c69",
+//       province: {
+//         uuid: "ee8d857c-78fe-11ef-bd77-9e4478916c69",
+//         name: "Bali",
+//       },
+//       external_id: 128,
+//       name: "Kabupaten Gianyar",
+//     },
+//     district: "Bojong",
+//     postal_code: "43222",
+//     detail_address: "Jl. ABC No. 123",
+//     address_note: "Dekat tugu pahlawan",
+//     type: "home",
+//   },
+//   {
+//     uuid: "953bc2be-79c5-11ef-b707-97a05ceb87e2",
+//     is_default: false,
+//     receiver_name: "Azis Hapidin",
+//     receiver_phone: "08888",
+//     city: {
+//       uuid: "ee8eb26c-78fe-11ef-bd77-9e4478916c69",
+//       province: {
+//         uuid: "ee8d857c-78fe-11ef-bd77-9e4478916c69",
+//         name: "Bali",
+//       },
+//       external_id: 128,
+//       name: "Kabupaten Gianyar",
+//     },
+//     district: "Bojong",
+//     postal_code: "43222",
+//     detail_address: "Jl. ABC No. 123",
+//     address_note: "Dekat tugu pahlawan",
+//     type: "home",
+//   },
+// ];
 
 const handleChange = () => {
   openEditor.value = true;
